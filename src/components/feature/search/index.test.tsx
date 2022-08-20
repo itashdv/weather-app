c255e57a0d48291ec225d8712c053e6b4cfe7c13
itashdv <itashcb@gmail.com>
2022-08-20 09:49:23 +0200

    Refactor search test code

--- src/components/feature/search/index.test.tsx
+++ src/components/feature/search/index.test.tsx
@@ -7,28 +7,24 @@ import { Search } from './index';
 const setup = (jsx: ReactElement) =>
   ({ user: userEvent.setup(), ...render(jsx) });
 
-it('has correct initial value', () => {
+it('has correct initial value on mount', () => {
   const { getByRole } = setup(<Search />);
 
-  const searchInput = getByRole('searchbox');
-
-  expect(searchInput).toHaveValue('');
+  expect(getByRole('searchbox')).toHaveValue('');
 });
 
 it('focuses and types correctly', async () => {
   const { user, getByRole } = setup(<Search />);
 
-  const searchInput = getByRole('searchbox');
+  await user.click(getByRole('searchbox'));
 
-  await user.click(searchInput);
-
-  expect(searchInput).toHaveFocus();
+  expect(getByRole('searchbox')).toHaveFocus();
 
   await user.keyboard('Botswana');
 
-  expect(searchInput).toHaveValue('Botswana');
+  expect(getByRole('searchbox')).toHaveValue('Botswana');
 
-  await user.clear(searchInput);
+  await user.clear(getByRole('searchbox'));
 
-  expect(searchInput).toHaveValue('');
+  expect(getByRole('searchbox')).toHaveValue('');
 });
